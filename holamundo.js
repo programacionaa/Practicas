ea3fa011c3b627543378a28b50a9cff95f0fde4e
programacionaa <reparacionaa@gmail.com>
2016-01-04 20:45:45 -0430

    Update holamundo.js

--- holamundo.js
+++ holamundo.js
@@ -1,7 +1,18 @@
 alert ("Hola Mundo");
 
-var hola = "Hola Mundo como esta hoy";
+var hola;
+hola = "Hola Mundo como esta hoy";
 documentwrite ("Esta seria la impresion de la Variable")+ hola;
 // El resultado seria igual a: Esta seria la impresion de la Vabiable Hola mundo como estas hoy 
 
 // Esto fue una prueba de como crear archivos con la plataforma github
+ 
+// vamos a crear variables
+
+ var helado;
+ helado ="Helado en Inglés Icecream";
+//sustituyendo el valor de la cadena de la variable helado por otro contenido
+helado = "IceCream";
+// ahora el valor a imprimir de la variable será IceCream 
+// A una variable no se le puede asignar el nombre que queramos hay que cumplir ciertas reglas como sólo puede contener letras números y guión bajo, no puede comenzar por números no puede haber espacio en separaciones de palabras lo que se denomina camel case lo cual es una notación que consiste en eliminar los espacios y poner en mayúsculas las primeras letras de cada palabra 
+// tampoco puede llamar a una variable como una palabra reservada si desea conocer todas esas palabras puede ir a http://www.ecmascript.org 
