5b507ee707716b09b77c259993d2ca08b63c29a4
programacionaa <reparacionaa@gmail.com>
2016-01-04 19:54:01 -0430

    Un codigo facil hola mundo
    Siempre se utiliza para probar plataformas

---
+++ holamundo.js
@@ -0,0 +1,7 @@
+alert ("Hola Mundo");
+
+var hola = "Hola Mundo como esta hoy";
+documentwrite ("Esta seria la impresion de la Variable")+ hola;
+// El resultado seria igual a: Esta seria la impresion de la Vabiable Hola mundo como estas hoy 
+
+// Esto fue una prueba de como crear archivos con la plataforma github
